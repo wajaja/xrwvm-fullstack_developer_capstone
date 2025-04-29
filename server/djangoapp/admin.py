e8ac720e38d3129ab8110182c02c8997f36570e9
wajaja <cedrickngeja@gmail.com>
2025-04-29 11:11:03 -0400

    ksks

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,5 +1,5 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 
 # Register your models here.
@@ -7,7 +7,10 @@
 # CarModelInline class
 
 # CarModelAdmin class
+admin.site.register(CarModel)
 
 # CarMakeAdmin class with CarModelInline
+admin.site.register(CarMake)
+
 
 # Register models here
