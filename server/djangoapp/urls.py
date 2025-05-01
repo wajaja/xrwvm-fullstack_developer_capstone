08dcc88dab1576586115f22b5114646be44ce2b1
Cedrick Ngeja <wajaja@users.noreply.github.com>
2025-05-01 02:15:22 +0100

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from django.views.generic import TemplateView
 from . import views
 
 app_name = 'djangoapp'
@@ -11,17 +10,20 @@ urlpatterns = [
     path('registration/', views.registration, name='registration'),
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_request, name='logout'),
-
     # Car-related URLs
     path('get_cars/', views.get_cars, name='getcars'),
-
     # Dealer URLs
-    path('dealers/', views.get_dealerships, name='get_dealers'),
-    path('dealers/<str:state>/', views.get_dealerships, name='get_dealers_by_state'),
-    path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
-
+    path('dealers/', views.get_dealerships, 
+        name='get_dealers'),
+    path('dealers/<str:state>/', views.get_dealerships, 
+        name='get_dealers_by_state'),
+    path('dealer/<int:dealer_id>/', views.get_dealer_details, 
+        name='dealer_details'),
     # Review URLs
-    path('reviews/dealer/<int:dealer_id>/', views.get_dealer_reviews, name='dealer_reviews'),
-    path('add_review/', views.add_review, name='add_review'),
+    path('reviews/dealer/<int:dealer_id>/', 
+        views.get_dealer_reviews, 
+        name='dealer_reviews'),
+    path('add_review/', views.add_review, 
+        name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
