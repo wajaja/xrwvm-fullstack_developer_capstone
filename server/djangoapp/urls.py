a278da315c51bb5a7b1e1bfcd92cbbcf35098330
Cedrick Ngeja <wajaja@users.noreply.github.com>
2025-05-01 01:37:45 +0100

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,31 +1,27 @@
-# Uncomment the imports before you add the code
-from django.views.generic import TemplateView
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView
 from . import views
 
 app_name = 'djangoapp'
+
 urlpatterns = [
-    # # path for registration
+    # Authentication URLs
     path('registration/', views.registration, name='registration'),
-
-    # path for login
-    path(route='login', view=views.login_user, name='login'),
-
+    path('login/', views.login_user, name='login'),
     path('logout/', views.logout_request, name='logout'),
 
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
-    # path for dealer reviews view
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    # Car-related URLs
+    path('get_cars/', views.get_cars, name='getcars'),
 
-    # path for add a review view
-        
-    path(route='add_review', view=views.add_review, name='add_review'),
+    # Dealer URLs
+    path('dealers/', views.get_dealerships, name='get_dealers'),
+    path('dealers/<str:state>/', views.get_dealerships, name='get_dealers_by_state'),
+    path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
 
-    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
-
-    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    # Review URLs
+    path('reviews/dealer/<int:dealer_id>/', views.get_dealer_reviews, name='dealer_reviews'),
+    path('add_review/', views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
