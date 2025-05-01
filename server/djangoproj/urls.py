c55a71f5d71d3edfe96df6cbb5491f0934a90dc7
Cedrick Ngeja <wajaja@users.noreply.github.com>
2025-05-01 01:40:49 +0100

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -1,18 +1,7 @@
-"""djangoproj URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+djangoproj URL Configuration
+"""
+
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
@@ -20,14 +9,27 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    # Admin site
     path('admin/', admin.site.urls),
-    path('djangoapp/', include('djangoapp.urls')),
-    path('contact/', TemplateView.as_view(template_name="Contact.html")),
-    path('about/', TemplateView.as_view(template_name="About.html")),
-    path('login/', TemplateView.as_view(template_name="index.html")),
-    path('register/', TemplateView.as_view(template_name="index.html")),
-    path('', TemplateView.as_view(template_name="Home.html")),
-    path('dealers/', TemplateView.as_view(template_name="index.html")),
-    path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
-    path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
+    
+    # Django app API endpoints
+    path('api/', include('djangoapp.urls')),
+    
+    # Static pages
+    path('', TemplateView.as_view(template_name="Home.html"), name='home'),
+    path('about/', TemplateView.as_view(template_name="About.html"), name='about'),
+    path('contact/', TemplateView.as_view(template_name="Contact.html"), name='contact'),
+    
+    # Frontend routes (handled by React)
+    path('login/', TemplateView.as_view(template_name="index.html"), name='login'),
+    path('register/', TemplateView.as_view(template_name="index.html"), name='register'),
+    path('dealers/', TemplateView.as_view(template_name="index.html"), name='dealers'),
+    
+    # Dynamic frontend routes
+    path('dealer/<int:dealer_id>/', 
+         TemplateView.as_view(template_name="index.html"), 
+         name='dealer_detail'),
+    path('postreview/<int:dealer_id>/', 
+         TemplateView.as_view(template_name="index.html"), 
+         name='post_review'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
